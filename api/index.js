68f4a6e20e35437dacd1ce72da40e89710d18195
tibendaerick <tibendaerick@gmail.com>
2026-09-04 10:58:25 +0300

    fix: replace serverless functions with pure static html redirects

--- api/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-    // Get client name from link query or default to hbhotel_hoima
-    const clientId = req.query.clientId || 'hbhotel_hoima';
-    
-    // Redirect directly to the hotel home page
-    res.redirect(302, `/h/${clientId}/index.html`);
-};
